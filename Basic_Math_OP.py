8b5adbf40674b9f47159f8666c5c82c9bb653a69
MohammadZaid_30 <jamadarmohammadzaid7@gmail.com>
2026-01-28 17:37:58 +0530

    Add files via upload

---
+++ Basic_Math_OP.py
@@ -0,0 +1,39 @@
+# Performing Basic Mathematical Operations
+
+# Addition
+
+# Taking two numbers from user.
+
+num1 = int(input("Enter the fist number: "))
+num2 = int(input("Enter the second number: "))
+
+print()
+
+# Using Arithmetic Operators
+
+add = (num1) + (num2)
+sub =(num1) -  (num2)
+mul = (num1) *  (num2)
+div =float (num1) / float (num2)
+
+print("Addition: ", add,)
+print("Subtraction: ", sub,)
+print("Multiplication: ", mul,)
+print("Division: ", div)
+
+
+
+""" 
+
+Result
+Enter the fist number: 4
+Enter the second number: 8
+
+Addition: 12
+Subtraction: -4
+Multiplication: 32
+Division: 0.5
+
+
+
+"""
---
+++ Greeting.py
@@ -0,0 +1,18 @@
+# Personalized Greeting
+
+# Taking User Input
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print()
+
+full_name = first_name + " " + last_name   #Combining first_name and last_name
+
+print("Hello,",full_name,",Welcome to Python Greetings!")
+
+""""
+Result
+Hello, XYZ, Welcome to Python Greetings!
+
+"""
